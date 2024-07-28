7d2e0f9539ef6cbb05b15263c9f1179b93c62e30
bachnvse <Bachnvhe172297@fpt.edu.vn>
2024-07-28 22:34:59 +0700

    add BE

---
+++ AdminManager/AdminManager/Entity/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProjectFS2.Service.Controller;
+using ProjectFS2.DBContext;
+using ProjectFS2.Entity;
+using ProjectFS2.Service.DataAccess;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ProjectFS2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IEmployeeController _employeeController;
+        public EmployeeController(IEmployeeController employeeController)
+        {
+            _employeeController = employeeController;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> addEmployee(string username, string password, string roleName, string departmentName)
+        {
+            
+            var token = HttpContext.Session.GetString("token");
+            int userCurrentId = (int)HttpContext.Session.GetInt32("userId");
+            string reponseAdd = await _employeeController.addEmployee(token, userCurrentId, username, password, roleName, departmentName);
+            if (reponseAdd != null)
+                return RedirectToAction("Index", "Home");
+            else return BadRequest("Fail add");
+            
+        }
+
+        [HttpPost]
+        public async Task<string> getEmployeeById(int id)
+        {
+            var token = HttpContext.Session.GetString("token");
+            return await _employeeController.getEmployeeById(token, id);
+        }
+    }
+}
---
+++ AdminManager/AdminManager/Entity/Users.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectFS2.Entity
+{
+    public class Users
+    {
+        [Key]
+        public int id { get; set; }
+        public string ?username { get; set; }
+        public string ?password { get; set; }
+        public string ?full_name { get; set; }
+        public int role_id { get; set; }
+        public int status { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public int created_by { get; set; }
+        public int updated_by { get; set;}
+        public Employee Employee { get; set; }
+
+
+    }
+}
