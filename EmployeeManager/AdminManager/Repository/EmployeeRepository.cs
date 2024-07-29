1692c380079faed6da26416b3499a52641b1fa4a
bachnvse <Bachnvhe172297@fpt.edu.vn>
2024-07-30 00:13:59 +0700

    add employeeRepository

--- EmployeeManager/AdminManager/Repository/EmployeeRepository.cs
+++ EmployeeManager/AdminManager/Repository/EmployeeRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace EmployeeManager.Repository
+﻿using AdminManager.OfficeDB;
+using ProjectFS2.Entity;
+
+namespace EmployeeManager.Repository
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : IEmployeeRepository
     {
+        private readonly ApplicationDbContext _officeDB;
+        public EmployeeRepository(ApplicationDbContext officeDB)
+        {
+            _officeDB = officeDB;
+        }
+        public void AddEmployee(Employee employee)
+        {
+            _officeDB.Employees.Add(employee);
+            _officeDB.SaveChanges();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = GetEmployeeById(id);
+            _officeDB.Employees.Remove(employee);
+            _officeDB.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _officeDB.Employees.ToList();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return (Employee)_officeDB.Employees.Where(x => x.EmployeeId == id);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            _officeDB.Employees.Update(employee);
+            _officeDB.SaveChanges();
+        }
     }
 }
